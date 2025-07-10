c20df93293a0f9886ef0359c4ac4159e1ee04bdb
Maxwell Hudson <mhudson8@wisc.edu>
2025-07-10 16:58:29 -0500

    Getting it working

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { HashRouter } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 import Search from './components/Search'
 import Browse from './components/Browse'
 

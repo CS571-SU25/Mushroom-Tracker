bd75c39d634c9700af321b637f258f435a8f57ac
Maxwell Hudson <mhudson8@wisc.edu>
2025-07-10 16:33:39 -0500

    First branch stuff

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: '/p0/',
+  base: '/mushroom-tracker/',
   build: {
     outDir: 'docs'
   }
